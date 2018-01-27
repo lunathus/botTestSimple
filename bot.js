3ca63329513ab85b998736875ac54bc5bcd98c42
lunathus <lunathus@gmail.com>
2018-01-26 22:43:18 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,9 +8,29 @@ client.on('ready', () => {
 client.on('message', message => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // !gw2
+            case 'gw2':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'NOOOOOOOOOO!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
 });
 
 // THIS  MUST  BE  THIS  WAY
