0c9771f7a747555970a1a35cf750a3f381ee8a85
lunathus <lunathus@gmail.com>
2018-01-26 23:11:50 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '!ping') {
-    	message.sendMessage('Pong');
+    	client.sendMessage(message, "Hello!");
   	}
 });
 
