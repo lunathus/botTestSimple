3e4c1c1df82661f9286d2fdac85d79f5cf9497b6
lunathus <lunathus@gmail.com>
2018-02-26 13:49:32 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on('message', message => {
   var voiceChannel = message.member.voiceChannel;
   voiceChannel.join().then(connection =>
   {
-     const dispatcher = connection.playFile('./Audio/asd.png');
+     const dispatcher = connection.playFile('./Audio/way.mp3');
      dispatcher.on("end", end => {
        voiceChannel.leave();
        });
