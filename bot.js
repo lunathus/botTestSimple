813f94fe56bf4972c5d0084e2991aa4d22248feb
lunathus <lunathus@gmail.com>
2018-02-26 15:15:24 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,15 +16,9 @@ client.on('message', message => {
   	}
   if (message.content === '!way')
   {
-  if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
+  voiceChannel.join()
+  .then(connection => console.log('Connected!'))
+  .catch(console.error);
    message.channel.send('Okay...4');
   }
 });
